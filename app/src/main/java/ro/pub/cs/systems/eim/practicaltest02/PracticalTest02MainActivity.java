7501ac2bb7ded9a86433c499b26655c80d4fa9ab
Remus Bompa <remus_stefan.bompa@stud.acs.pub.ro>
2020-05-20 12:55:13 +0300

    2-Interfata Grafica

--- app/src/main/java/ro/pub/cs/systems/eim/practicaltest02/PracticalTest02MainActivity.java
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest02/PracticalTest02MainActivity.java
@@ -2,12 +2,112 @@ package ro.pub.cs.systems.eim.practicaltest02;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+import ro.pub.cs.systems.eim.practicaltest02.general.Constants;
+import ro.pub.cs.systems.eim.practicaltest02.network.ClientThread;
+import ro.pub.cs.systems.eim.practicaltest02.network.ServerThread;
 
 public class PracticalTest02MainActivity extends AppCompatActivity {
+
+    // Server widgets
+    private EditText serverPortEditText = null;
+    private Button connectButton = null;
+
+    // Client widgets
+    private EditText clientAddressEditText = null;
+    private EditText clientPortEditText = null;
+    private EditText cityEditText = null;
+    private Spinner informationTypeSpinner = null;
+    private Button getWeatherForecastButton = null;
+    private TextView weatherForecastTextView = null;
+
+    private ServerThread serverThread = null;
+    private ClientThread clientThread = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practical_test02_main);
+
+        serverPortEditText = (EditText)findViewById(R.id.server_port_textedit);
+        connectButton = (Button)findViewById(R.id.button_connect);
+        connectButton.setOnClickListener(connectButtonClickListener);
+
+        clientAddressEditText = (EditText)findViewById(R.id.adr_client_textedit);
+        clientPortEditText = (EditText)findViewById(R.id.port_client_textedit);
+        cityEditText = (EditText)findViewById(R.id.oras_textedit);
+        informationTypeSpinner = (Spinner)findViewById(R.id.spinner);
+        getWeatherForecastButton = (Button)findViewById(R.id.button);
+        getWeatherForecastButton.setOnClickListener(getWeatherForecastButtonClickListener);
+        weatherForecastTextView = (TextView)findViewById(R.id.weather_forecast_text_view);
+    }
+
+    private ConnectButtonClickListener connectButtonClickListener = new ConnectButtonClickListener();
+    private class ConnectButtonClickListener implements Button.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            String serverPort = serverPortEditText.getText().toString();
+            if (serverPort == null || serverPort.isEmpty()) {
+                Toast.makeText(getApplicationContext(), "[MAIN ACTIVITY] Server port should be filled!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            serverThread = new ServerThread(Integer.parseInt(serverPort));
+            if (serverThread.getServerSocket() == null) {
+                Log.e(Constants.TAG, "[MAIN ACTIVITY] Could not create server thread!");
+                return;
+            }
+            serverThread.start();
+        }
+
+    }
+
+    private GetWeatherForecastButtonClickListener getWeatherForecastButtonClickListener = new GetWeatherForecastButtonClickListener();
+    private class GetWeatherForecastButtonClickListener implements Button.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            String clientAddress = clientAddressEditText.getText().toString();
+            String clientPort = clientPortEditText.getText().toString();
+            if (clientAddress == null || clientAddress.isEmpty()
+                    || clientPort == null || clientPort.isEmpty()) {
+                Toast.makeText(getApplicationContext(), "[MAIN ACTIVITY] Client connection parameters should be filled!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if (serverThread == null || !serverThread.isAlive()) {
+                Toast.makeText(getApplicationContext(), "[MAIN ACTIVITY] There is no server to connect to!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            String city = cityEditText.getText().toString();
+            String informationType = informationTypeSpinner.getSelectedItem().toString();
+            if (city == null || city.isEmpty()
+                    || informationType == null || informationType.isEmpty()) {
+                Toast.makeText(getApplicationContext(), "[MAIN ACTIVITY] Parameters from client (city / information type) should be filled", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            weatherForecastTextView.setText(Constants.EMPTY_STRING);
+
+            clientThread = new ClientThread(
+                    clientAddress, Integer.parseInt(clientPort), city, informationType, weatherForecastTextView
+            );
+            clientThread.start();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.i(Constants.TAG, "[MAIN ACTIVITY] onDestroy() callback method has been invoked");
+        if (serverThread != null) {
+            serverThread.stopThread();
+        }
+        super.onDestroy();
     }
 }
